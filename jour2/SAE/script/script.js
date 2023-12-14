97ea11406a078db616994f0d56885c72a858d058
SorenGAUDRE <153618025+SorenGAUDRE@users.noreply.github.com>
2023-12-14 11:01:36 +0100

    Update script.js

--- jour2/SAE/script/script.js
+++ jour2/SAE/script/script.js
@@ -1,25 +1,18 @@
+// script.js
 $(document).ready(function () {
-  // Effet Parallaxe
   var scrollTop = window.scrollY;
   $(window).scroll(function () {
     $('.parallax').css('background-position-y', -(scrollTop * 0.3) + 'px');
   });
 
-  // Style de la barre de navigation lors du défilement
-  window.addEventListener('scroll', function () {
-    var navbar = document.querySelector('.navbar');
-    var scrollTop = window.scrollY;
-    if (scrollTop === 0) {
-      navbar.classList.add('fixed-top', 'transparent-bg');
-    } else {
-      navbar.classList.remove('fixed-top', 'transparent-bg');
-    }
-  });
+ 
 
-  // Validation du formulaire de connexion
+  // Vérification des permissions lors du chargement de la page
   var userType = getQueryParam('type');
+
   var loginForm = document.getElementById('loginForm');
   if (loginForm) {
+    // Si c'est la page de connexion, ajoutez le gestionnaire d'événements pour la validation
     loginForm.addEventListener('submit', function (event) {
       event.preventDefault();
       validateLogin();
@@ -27,28 +20,48 @@ $(document).ready(function () {
   }
 });
 
-// Fonctions utilitaires
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('.navbar');
+  var scrollTop = window.scrollY;
+  if (scrollTop === 0) {
+    navbar.classList.add('fixed-top', 'transparent-bg');
+  } else {
+    navbar.classList.remove('fixed-top', 'transparent-bg');
+  }
+});
+
+// Fonction pour obtenir la valeur d'un paramètre d'URL
 function getQueryParam(name) {
   var urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(name);
 }
 
+// Fonction pour vérifier les permissions en fonction du type d'utilisateur
 function checkPermissions(userType) {
   switch (userType) {
     case 'entreprise':
+      // Autorisation pour l'utilisateur de type entreprise
+      return true;
     case 'association':
+      // Autorisation pour l'utilisateur de type association
+      return true;
     case 'mairie':
+      // Autorisation pour l'utilisateur de type mairie
+      return true;
     case 'habitant':
+      // Autorisation pour l'utilisateur de type habitant
       return true;
     default:
       return false;
   }
 }
 
-// Validation de la connexion
+
+// Fonction pour valider la connexion
 function validateLogin() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
+
   var userType;
 
   if (username === 'entreprise2024' && password === 'JO2024') {
@@ -60,10 +73,12 @@ function validateLogin() {
   } else if (username == 'habitant2024' && password == 'JO2024') {
     userType = 'habitant';
   } else {
+    // Afficher une pop-up d'erreur en utilisant Bootstrap
     alert('Identifiant ou mot de passe incorrect');
-    return;
+    return; // Arrêter l'exécution si l'authentification échoue
   }
 
+  // Vérifier les permissions avant de rediriger vers la page de questionnaire
   if (checkPermissions(userType)) {
     var questionnairePage = 'questionnaire_' + userType + '.html?type=' + userType;
     window.location.href = questionnairePage;
@@ -73,52 +88,55 @@ function validateLogin() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Compte à rebours pour les Jeux Olympiques 2024
-  const olympicsDate = new Date('26 juillet 2024 00:00:00 GMT+0000');
+  // Date des Jeux Olympiques 2024 à Paris
+  const olympicsDate = new Date('July 26, 2024 00:00:00 GMT+0000');
 
   function updateCountdown() {
-    const currentDate = new Date();
-    const timeDifference = olympicsDate - currentDate;
-    const seconds = Math.floor(timeDifference / 1000) % 60;
-    const minutes = Math.floor(timeDifference / 1000 / 60) % 60;
-    const hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 365;
-    const months = Math.floor(days / 30);
-    const remainingDaysInMonth = days % 30;
-    const years = Math.floor(days / 365);
+      const currentDate = new Date();
+      const timeDifference = olympicsDate - currentDate;
 
-    document.getElementById('years').textContent = years;
-    document.getElementById('months').textContent = months;
-    document.getElementById('days').textContent = remainingDaysInMonth;
-    document.getElementById('hours').textContent = hours;
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
+      const seconds = Math.floor(timeDifference / 1000) % 60;
+      const minutes = Math.floor(timeDifference / 1000 / 60) % 60;
+      const hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 365;
+
+      // Calculer les mois et les jours restants dans le mois
+      const months = Math.floor(days / 30);
+      const remainingDaysInMonth = days % 30;
+
+      const years = Math.floor(days / 365);
+
+      document.getElementById('years').textContent = years;
+      document.getElementById('months').textContent = months;
+      document.getElementById('days').textContent = remainingDaysInMonth;
+      document.getElementById('hours').textContent = hours;
+      document.getElementById('minutes').textContent = minutes;
+      document.getElementById('seconds').textContent = seconds;
   }
 
+  // Mettez à jour le compte à rebours toutes les secondes
   setInterval(updateCountdown, 1000);
+
+  // Mettez à jour le compte à rebours lors du chargement initial de la page
   updateCountdown();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Anneaux de navigation
   var ringBlue = document.querySelector(".ring-blue.ring-clickable");
   var ringRed = document.querySelector(".ring-red.ring-clickable");
   var ringGreen = document.querySelector(".ring-green.ring-clickable");
   var ringYellow = document.querySelector(".ring-yellow.ring-clickable");
-
   ringBlue.addEventListener("click", function () {
     window.location.href = "index.html";
   });
-
   ringRed.addEventListener("click", function () {
     window.location.href = "financement.html";
   });
-
   ringGreen.addEventListener("click", function () {
     window.location.href = "faq.html";
   });
-
   ringYellow.addEventListener("click", function () {
     window.location.href = "presentation.html";
   });
 });
+
