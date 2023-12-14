cb53016d4184c19dcd7de9219d4145ae5dc68c84
SoulLikePlayer <152029535+SoulLikePlayer@users.noreply.github.com>
2023-12-14 12:14:45 +0100

    Update script.js

--- jour2/SAE/script/script.js
+++ jour2/SAE/script/script.js
@@ -1,18 +1,21 @@
 // script.js
 $(document).ready(function () {
-  var scrollTop = window.scrollY;
+  // Récupérer le userType du stockage local
+  var storedUserType = localStorage.getItem('userType');
+
+  // Vérifier si le userType est déjà stocké
+  if (storedUserType) {
+    // Si oui, rediriger vers la page spécifique avec le userType
+    redirectToPage(storedUserType);
+  }
+
   $(window).scroll(function () {
-    $('.parallax').css('background-position-y', -(scrollTop * 0.3) + 'px');
+    // Correction pour le parallax
+    $('.parallax').css('background-position-y', -(window.scrollY * 0.3) + 'px');
   });
-
- 
-
-  // Vérification des permissions lors du chargement de la page
-  var userType = getQueryParam('type');
 
   var loginForm = document.getElementById('loginForm');
   if (loginForm) {
-    // Si c'est la page de connexion, ajoutez le gestionnaire d'événements pour la validation
     loginForm.addEventListener('submit', function (event) {
       event.preventDefault();
       validateLogin();
@@ -21,8 +24,9 @@ $(document).ready(function () {
 });
 
 window.addEventListener('scroll', function () {
-  var navbar = document.querySelector('.navbar');
   var scrollTop = window.scrollY;
+  var navbar = document.querySelector('.navbar');
+
   if (scrollTop === 0) {
     navbar.classList.add('fixed-top', 'transparent-bg');
   } else {
@@ -30,34 +34,24 @@ window.addEventListener('scroll', function () {
   }
 });
 
-// Fonction pour obtenir la valeur d'un paramètre d'URL
 function getQueryParam(name) {
   var urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(name);
 }
 
-// Fonction pour vérifier les permissions en fonction du type d'utilisateur
 function checkPermissions(userType) {
   switch (userType) {
     case 'entreprise':
-      // Autorisation pour l'utilisateur de type entreprise
-      return true;
     case 'association':
-      // Autorisation pour l'utilisateur de type association
-      return true;
     case 'mairie':
-      // Autorisation pour l'utilisateur de type mairie
-      return true;
     case 'habitant':
-      // Autorisation pour l'utilisateur de type habitant
-      return true;
+    case 'admin':
+      return true; // Autoriser ces types d'utilisateurs
     default:
       return false;
   }
 }
 
-
-// Fonction pour valider la connexion
 function validateLogin() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
@@ -66,41 +60,67 @@ function validateLogin() {
 
   if (username === 'entreprise2024' && password === 'JO2024') {
     userType = 'entreprise';
-  } else if (username == 'association2024' && password == 'JO2024') {
+  } else if (username === 'association2024' && password === 'JO2024') {
     userType = 'association';
-  } else if (username == 'mairie2024' && password == 'JO2024') {
+  } else if (username === 'mairie2024' && password === 'JO2024') {
     userType = 'mairie';
-  } else if (username == 'habitant2024' && password == 'JO2024') {
+  } else if (username === 'habitant2024' && password === 'JO2024') {
     userType = 'habitant';
+  } else if (username === 'Administrateur24' && password === 'JO2024') {
+    userType = 'admin';
   } else {
-    // Afficher une pop-up d'erreur en utilisant Bootstrap
     alert('Identifiant ou mot de passe incorrect');
-    return; // Arrêter l'exécution si l'authentification échoue
+    return;
   }
 
-  // Vérifier les permissions avant de rediriger vers la page de questionnaire
+  // Stocker le userType dans le localStorage
+  localStorage.setItem('userType', userType);
+
+  // Rediriger vers la page spécifique avec le userType
+  redirectToPage(userType);
+}
+
+function redirectToPage(userType) {
   if (checkPermissions(userType)) {
-    var questionnairePage = 'questionnaire_' + userType + '.html?type=' + userType;
-    window.location.href = questionnairePage;
+    var destinationPage;
+    switch (userType) {
+      case 'admin':
+        destinationPage = 'Statistique.html'
+        window.location.href = destinationPage;
+        break;
+      case 'entreprise':
+        destinationPage = 'questionnaire_entreprise.html?type=' + userType;
+        window.location.href = destinationPage;
+        break;
+      case 'association':
+        destinationPage = 'questionnaire_association.html?type=' + userType;
+        window.location.href = destinationPage;
+        break;
+      case 'mairie':
+        destinationPage = 'questionnaire_mairie.html?type=' + userType;
+        window.location.href = destinationPage;
+        break;
+      case 'habitant':
+        destinationPage = 'questionnaire_habitant.html?type=' + userType;
+        window.location.href = destinationPage;
+        break;
+      default:
+        destinationPage = 'index.html';
+    }
   } else {
     alert('Vous n\'avez pas les permissions nécessaires pour accéder à cette page');
   }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Date des Jeux Olympiques 2024 à Paris
   const olympicsDate = new Date('July 26, 2024 00:00:00 GMT+0000');
-
   function updateCountdown() {
       const currentDate = new Date();
       const timeDifference = olympicsDate - currentDate;
-
       const seconds = Math.floor(timeDifference / 1000) % 60;
       const minutes = Math.floor(timeDifference / 1000 / 60) % 60;
       const hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
       const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 365;
-
-      // Calculer les mois et les jours restants dans le mois
       const months = Math.floor(days / 30);
       const remainingDaysInMonth = days % 30;
 
@@ -113,11 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('minutes').textContent = minutes;
       document.getElementById('seconds').textContent = seconds;
   }
-
-  // Mettez à jour le compte à rebours toutes les secondes
   setInterval(updateCountdown, 1000);
-
-  // Mettez à jour le compte à rebours lors du chargement initial de la page
   updateCountdown();
 });
 
@@ -126,6 +142,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var ringRed = document.querySelector(".ring-red.ring-clickable");
   var ringGreen = document.querySelector(".ring-green.ring-clickable");
   var ringYellow = document.querySelector(".ring-yellow.ring-clickable");
+  var ringBlack = document.querySelector(".ring-black.ring-clickable");
+
   ringBlue.addEventListener("click", function () {
     window.location.href = "index.html";
   });
@@ -139,4 +157,3 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "presentation.html";
   });
 });
-
